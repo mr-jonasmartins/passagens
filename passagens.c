464e624ff89aa311217aa06b0eb80ad0e0e9ad75
Jonas Santos <jonas@MacBook-Air-de-Jonas.local>
2021-07-08 16:32:32 -0300

    concluindo em c

--- passagens.c
+++ passagens.c
@@ -3,12 +3,13 @@
 
 int include(int val, int arr[], int tam);
 
-int main(void) {
+int main(void)
+{
 
-    char *tentativas[100] = {"A>C","A>C","A>C","B>D","B>D","B>D","B>D","B>D","A>B","A>B","A>B","A>B","B>C","B>C","B>C","B>C","B>C","A>D","A>D","A>D","A>D","A>D","A>D","A>D","A>D","A>D","C>D","C>D","C>D","C>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>C","B>C","B>C","A>C","A>B","A>B","B>C","B>C","B>C","A>C","C>D","C>D","C>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>D","A>B","A>B","A>C","A>C","A>C","C>D","C>D","A>B","A>B","A>D"};
+    char *tentativas[100] = {"A>C", "A>C", "A>C", "B>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>B", "A>B", "B>C", "B>C", "B>C", "B>C", "B>C", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "B>C", "B>C", "B>C", "A>C", "A>B", "A>B", "B>C", "B>C", "B>C", "A>C", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>C", "A>C", "A>C", "C>D", "C>D", "A>B", "A>B", "A>D"};
 
     int ab[45];
-    int bc[45]; 
+    int bc[45];
     int cd[45];
     int poltronas = 5;
     int count = 0;
@@ -16,23 +17,27 @@ int main(void) {
     int bc_tamanho = 0;
     int cd_tamanho = 0;
 
-    for(int i = 0 ; i < 100 ; i++) {
+    for (int i = 0; i < 100; i++)
+    {
 
         count = count + 1;
 
-        if (strcmp("A>B", tentativas[i]) == 0 && ab_tamanho < poltronas ) {
+        if (strcmp("A>B", tentativas[i]) == 0 && ab_tamanho < poltronas)
+        {
             ab[count] = count;
             ab_tamanho = ab_tamanho + 1;
         }
 
-        if (strcmp("A>C", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas) {
+        if (strcmp("A>C", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas)
+        {
             ab[count] = count;
             ab_tamanho = ab_tamanho + 1;
             bc[count] = count;
             bc_tamanho = bc_tamanho + 1;
         }
 
-        if (strcmp("A>D", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas && cd_tamanho < poltronas) {
+        if (strcmp("A>D", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas && cd_tamanho < poltronas)
+        {
             ab[count] = count;
             ab_tamanho = ab_tamanho + 1;
             bc[count] = count;
@@ -41,36 +46,37 @@ int main(void) {
             cd_tamanho = cd_tamanho + 1;
         }
 
-        if (strcmp("B>C", tentativas[i]) == 0 && bc_tamanho < poltronas) {
+        if (strcmp("B>C", tentativas[i]) == 0 && bc_tamanho < poltronas)
+        {
             bc[count] = count;
             bc_tamanho = bc_tamanho + 1;
         }
 
-        if (strcmp("B>D", tentativas[i]) == 0 && bc_tamanho < poltronas && cd_tamanho < poltronas) {
+        if (strcmp("B>D", tentativas[i]) == 0 && bc_tamanho < poltronas && cd_tamanho < poltronas)
+        {
             bc[count] = count;
             bc_tamanho = bc_tamanho + 1;
             cd[count] = count;
             cd_tamanho = cd_tamanho + 1;
         }
 
-        if (strcmp("C>D", tentativas[i]) == 0 && cd_tamanho < poltronas) {
+        if (strcmp("C>D", tentativas[i]) == 0 && cd_tamanho < poltronas)
+        {
             cd[count] = count;
             cd_tamanho = cd_tamanho + 1;
         }
 
-        if (include(count, ab, ab_tamanho)) {
-              printf("ok");
-          }
+    // ainda finalizando o include
 
     }
-
 }
 
-int include(int val, int arr[], int tam) {
+int include(int val, int arr[], int tam)
+{
     int i;
-    for(i = 0; i < tam; i++)
+    for (i = 0; i < 45; i++)
     {
-        if(arr[i] == val)
+        if (arr[i] == val)
             return 1;
     }
     return 0;
