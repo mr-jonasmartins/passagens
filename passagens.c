56fa14d2750a8feed47ff7c30bc4c2d3e4b15426
Jonas Santos <mr.jonasmartins@gmail.com>
2022-11-19 11:46:57 -0300

    c concluido

--- passagens.c
+++ passagens.c
@@ -1,83 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 
-int include(int val, int arr[], int tam);
+int main() {
 
-int main(void)
-{
+    char *tentativas[] = {"A>C","A>C","A>C","B>D","B>D","B>D","B>D","B>D","A>B","A>B","A>B","A>B","B>C","B>C","B>C","B>C","B>C","A>D","A>D","A>D",
+                          "A>D","A>D","A>D","A>D","A>D","A>D","C>D","C>D","C>D","C>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>C","B>C",
+                          "B>C","A>C","A>B","A>B","B>C","B>C","B>C","A>C","C>D","C>D","C>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","A>B",
+                          "A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>B","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C",
+                          "A>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>D","A>B","A>B","A>C","A>C","A>C","C>D","C>D","A>B","A>B","A>D"};
 
-    char *tentativas[100] = {"A>C", "A>C", "A>C", "B>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>B", "A>B", "B>C", "B>C", "B>C", "B>C", "B>C", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "A>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "B>C", "B>C", "B>C", "A>C", "A>B", "A>B", "B>C", "B>C", "B>C", "A>C", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>B", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>C", "A>D", "C>D", "C>D", "C>D", "C>D", "B>D", "B>D", "B>D", "B>D", "B>D", "A>B", "A>B", "A>C", "A>C", "A>C", "C>D", "C>D", "A>B", "A>B", "A>D"};
+    int ab_tamanho = 0, bc_tamanho = 0, cd_tamanho = 0, poltronas = 45;
 
-    int ab[45];
-    int bc[45];
-    int cd[45];
-    int poltronas = 5;
-    int count = 0;
-    int ab_tamanho = 0;
-    int bc_tamanho = 0;
-    int cd_tamanho = 0;
+    int tentativas_tamanho = sizeof(tentativas) / sizeof(tentativas[0]);
 
-    for (int i = 0; i < 100; i++)
-    {
-
-        count = count + 1;
-
-        if (strcmp("A>B", tentativas[i]) == 0 && ab_tamanho < poltronas)
-        {
-            ab[count] = count;
+    for( int i=0; i < tentativas_tamanho; i++ ) {
+    
+        if (strcmp("A>B", tentativas[i]) == 0 && ab_tamanho < poltronas) {
             ab_tamanho = ab_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-        if (strcmp("A>C", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas)
-        {
-            ab[count] = count;
+        else if (strcmp("A>C", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas) {
             ab_tamanho = ab_tamanho + 1;
-            bc[count] = count;
             bc_tamanho = bc_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-        if (strcmp("A>D", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas && cd_tamanho < poltronas)
-        {
-            ab[count] = count;
+        else if (strcmp("A>C", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas) {
             ab_tamanho = ab_tamanho + 1;
-            bc[count] = count;
             bc_tamanho = bc_tamanho + 1;
-            cd[count] = count;
-            cd_tamanho = cd_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-        if (strcmp("B>C", tentativas[i]) == 0 && bc_tamanho < poltronas)
-        {
-            bc[count] = count;
+        else if (strcmp("A>D", tentativas[i]) == 0 && ab_tamanho < poltronas && bc_tamanho < poltronas && cd_tamanho < poltronas) {
+            ab_tamanho = ab_tamanho + 1;
             bc_tamanho = bc_tamanho + 1;
+            cd_tamanho = cd_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-        if (strcmp("B>D", tentativas[i]) == 0 && bc_tamanho < poltronas && cd_tamanho < poltronas)
-        {
-            bc[count] = count;
+        else if (strcmp("B>C", tentativas[i]) == 0 && bc_tamanho < poltronas) {
             bc_tamanho = bc_tamanho + 1;
-            cd[count] = count;
-            cd_tamanho = cd_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-        if (strcmp("C>D", tentativas[i]) == 0 && cd_tamanho < poltronas)
-        {
-            cd[count] = count;
+        else if (strcmp("B>D", tentativas[i]) == 0 && bc_tamanho < poltronas && cd_tamanho < poltronas) {
+            bc_tamanho = bc_tamanho + 1;
             cd_tamanho = cd_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
         }
 
-    // ainda finalizando o include
+        else if (strcmp("C>D", tentativas[i]) == 0 && cd_tamanho < poltronas) {
+            cd_tamanho = cd_tamanho + 1;
+            printf("%d - %s - VENDER \n", i+1, tentativas[i]);
+        }     
+
+        else {
+            printf("%d - %s - NAO VENDER \n", i+1, tentativas[i]);
+        }  
 
     }
-}
 
-int include(int val, int arr[], int tam)
-{
-    int i;
-    for (i = 0; i < 45; i++)
-    {
-        if (arr[i] == val)
-            return 1;
-    }
-    return 0;
 }
